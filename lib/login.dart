cc19ed62e3ab5e34f245fb8324fc1a37c650718b
eessaa007766 <090kml090@gmail.com>
2023-02-26 05:11:50 +0300

    Library-management

--- lib/login.dart
+++ lib/login.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:sizer/sizer.dart';
+import 'package:lottie/lottie.dart';
+//  import 'package:sizer/sizer.dart';
 
 class login extends StatefulWidget {
   const login({Key? key}) : super(key: key);
@@ -12,167 +13,106 @@ class _loginState extends State<login> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.indigo.shade900,
-      body: ListView(
+      // backgroundColor: Colors.indigo.shade900,
+      body: Column(
         children: [
-          Container(
-            height: 20.h,
-            width: double.infinity,
-            // color: Colors.red,
+          Expanded(flex: 2, child: Lottie.asset('inm/o.json')),
+          SizedBox(
+            height: 40,
+          ),
+          Row(
+            children: [
+              Expanded(child: Container()),
+              Expanded(
+                flex: 5,
+                child: TextFormField(
+                  keyboardType: TextInputType.number,
+                  onSaved: (val) {
+                    //     myemail = val!;
+                  },
+                  maxLength: 30,
+                  decoration: InputDecoration(
+                      filled: true,
+                      fillColor: Colors.white,
+                      labelText: "كلمة مرور المدير",
+                      labelStyle: TextStyle(color: Colors.black, fontSize: 14),
+                      suffixStyle: TextStyle(color: Colors.blue),
+                      hintStyle: TextStyle(color: Colors.blue),
+                      border:
+                          OutlineInputBorder(borderSide: BorderSide(width: 1)),
+                      prefixIcon: Icon(
+                        Icons.text_fields,
+                        color: Colors.blue,
+                      )),
+                ),
+              ),
+              Expanded(child: Container()),
+            ],
+          ),
+          Expanded(
             child: Row(
               children: [
+                Expanded(child: Container()),
                 Expanded(
-                  flex: 6,
                   child: Container(
-                    // color: Colors.white,
-                    child: Padding(
-                      padding: EdgeInsets.only(right: 26.w),
-                      child: IconButton(
-                        color: Colors.white,
-                        icon: Icon(
-                          Icons.arrow_back_ios_new_sharp,
-                          size: 27.sp,
+                    decoration: BoxDecoration(
+                      boxShadow: [
+                        BoxShadow(
+                          offset: Offset(0, 0),
+                          color: Colors.grey,
+                          blurRadius: 1,
                         ),
-                        onPressed: () {},
+                      ],
+                      border: Border.all(width: 0.8, color: Colors.black),
+                      color: Colors.white70,
+                      borderRadius: BorderRadius.circular(60),
+                    ),
+                    // width: 85,
+                    child: MaterialButton(
+                      child: Text(
+                        "تسجيل دخول",
+                        style: TextStyle(
+                            fontWeight: FontWeight.w700,
+                            fontSize: 18,
+                            color: Colors.blue.shade900),
                       ),
+                      onPressed: () async {
+                        //    Navigator.of(context).pushNamed("page1");
+                      },
                     ),
                   ),
                 ),
+                Expanded(child: Container()),
                 Expanded(
-                  flex: 4,
                   child: Container(
-                    //  color: Colors.black,
-                    height: 12.h,
-                    // width: 10.w,
                     decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(170),
-                        // borderRadius: BorderRadius.only(topLeft:Radius.circular(60) ),
-                        image: DecorationImage(
-                            image: AssetImage("assets/images/s2.jpg"),
-                            fit: BoxFit.fill)),
-                  ),
-                ),
-                Expanded(
-                    flex: 5,
-                    child: Container(
-                        //   color: Colors.yellow,
-                        )),
-              ],
-            ),
-          ),
-          ///////////////////////////////////////////////////////////////////////////////////////
-          Container(
-            //  height: 80.h,
-            // color: Colors.white,
-
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                  image: AssetImage(
-                    "assets/images/s3.jpg",
-                  ),
-                  colorFilter: ColorFilter.mode(
-                      Colors.black.withOpacity(0.2), BlendMode.dstATop),
-                  fit: BoxFit.fill),
-              color: Colors.white,
-              borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(60), topRight: Radius.circular(60)),
-            ),
-            child: Column(
-              children: [
-                SizedBox(
-                  height: 5.h,
-                ),
-                Container(
-                  height: 1.h,
-                  width: double.infinity,
-                ),
-                SizedBox(
-                  height: 12.h,
-                ),
-                Container(
-                  padding: EdgeInsets.all(8),
-                  child: Form(
-                      //  key: formstate,
-                      child: Column(
-                    children: [
-                      TextFormField(
-                        keyboardType: TextInputType.number,
-                        onSaved: (val) {
-                          //     myemail = val!;
-                        },
-                        maxLength: 30,
-                        decoration: InputDecoration(
-                            filled: true,
-                            fillColor: Colors.white,
-                            labelText: " الدخول id",
-                            labelStyle:
-                                TextStyle(color: Colors.black, fontSize: 14.sp),
-                            suffixStyle: TextStyle(color: Colors.blue),
-                            hintStyle: TextStyle(color: Colors.blue),
-                            border: OutlineInputBorder(
-                                borderSide: BorderSide(width: 1)),
-                            prefixIcon: Icon(
-                              Icons.text_fields,
-                              color: Colors.blue,
-                            )),
-                      ),
-                      SizedBox(
-                        height: 7.h,
-                      ),
-                      Container(
-                        height: 8.h,
-                        child: Container(
-                          decoration: BoxDecoration(
-                            boxShadow: [
-                              BoxShadow(
-                                offset: Offset(0, 0),
-                                color: Colors.grey,
-                                blurRadius: 1,
-                              ),
-                            ],
-                            border:
-                                Border.all(width: 0.5.w, color: Colors.black),
-                            color: Colors.white70,
-                            borderRadius: BorderRadius.circular(60),
-                          ),
-                          width: 85.w,
-                          child: MaterialButton(
-                            child: Text(
-                              "تسجيل دخول",
-                              style: TextStyle(
-                                  fontWeight: FontWeight.w700,
-                                  fontSize: 18.sp,
-                                  color: Colors.blue.shade900),
-                            ),
-                            onPressed: () async {},
-                          ),
+                      boxShadow: [
+                        BoxShadow(
+                          offset: Offset(0, 0),
+                          color: Colors.grey,
+                          blurRadius: 1,
                         ),
+                      ],
+                      border: Border.all(width: 0.8, color: Colors.black),
+                      color: Colors.white70,
+                      borderRadius: BorderRadius.circular(60),
+                    ),
+                    // width: 85,
+                    child: MaterialButton(
+                      child: Text(
+                        "تخطي",
+                        style: TextStyle(
+                            fontWeight: FontWeight.w700,
+                            fontSize: 18,
+                            color: Colors.blue.shade900),
                       ),
-                    ],
-                  )),
-                ),
-                SizedBox(
-                  height: 11.h,
-                ),
-                Container(
-                  //   color: Colors.red,
-                  height: 12.5.h,
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                    boxShadow: [
-                      BoxShadow(
-                        offset: Offset(0, 0),
-                        color: Colors.grey,
-                        blurRadius: 1,
-                      ),
-                    ],
-                    border: Border.all(width: 0.3.w, color: Colors.black),
-                    color: Colors.indigo.shade900,
-                    borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(30),
-                        topRight: Radius.circular(30)),
+                      onPressed: () async {
+                        Navigator.of(context).pushNamed("page1");
+                      },
+                    ),
                   ),
-                )
+                ),
+                Expanded(child: Container()),
               ],
             ),
           ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:mktbh/SplashScreen.dart';
+import 'package:mktbh/users/page1.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
+///  git commit -m "Library-management"
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.brown,
-      ),
-      home: SplashScreen(),
-    );
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.brown,
+        ),
+        home: SplashScreen(),
+        routes: {
+          "page1": (context) => page1(),
+        });
   }
 }
